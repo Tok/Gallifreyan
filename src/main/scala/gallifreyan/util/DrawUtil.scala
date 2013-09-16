c885ddef2ffac2f557d6a5e5580ca4901b34bcd9
lsteiger4@gmail.com <lsteiger4@gmail.com>
2013-09-16 12:35:25 +0200

    Line connectors calculated and highlighted.

--- src/main/scala/gallifreyan/util/DrawUtil.scala
+++ src/main/scala/gallifreyan/util/DrawUtil.scala
@@ -22,7 +22,8 @@ import gallifreyan.engine.characters.Punctation
 import gallifreyan.engine.characters.Vowel
 
 object DrawUtil {
-  val conceptMode = false
+  val conceptMode = true
+  
   val LINE_WIDTH = 2
   val HALF_LINE = LINE_WIDTH / 2
   val STROKE = new BasicStroke(LINE_WIDTH)
@@ -35,12 +36,23 @@ object DrawUtil {
     g2d.setBackground(bg)
     g2d.setStroke(STROKE)
     g2d.clearRect(0, 0, Size.width, Size.height)
-    drawCircle(g2d, getWordCircle, false)
+    val wc = getWordCircle
+    drawCircle(g2d, wc, false)
     if (!sent.isEmpty) {
       val rot = 360D / sent.size
+      val rots = (1 to sent.size).map(rot * _).reverse.toList
       val sizeRatio = CalcUtil.getSizeRatio(sent.size)
-      sent.foreach(drawSyllable(g2d, _, rot, sizeRatio))
+      val connectors: List[Set[Coord]] = sent.map(drawSyllable(g2d, _, rot, sizeRatio))
+ 
       g2d.rotate(Math.toRadians(rot * sent.size))
+
+      val zipped: List[(Set[Coord], Double)] = connectors.zip(rots)
+
+      def fixConns(coords: Set[Coord], rot: Double): Set[Coord] = coords.map(getRotatedDot(_, wc.center, rot))
+      val fixedConns: List[Set[Coord]] = zipped.map(cr => fixConns(cr._1, cr._2))
+      val flatConns: Set[Coord] = fixedConns.flatten.toSet
+
+      flatConns.foreach(highlightLineConnector(g2d, _))
     }
     if (addText) {
       g2d.drawString(sent.map(_.mkString).mkString, 10, FONT.getSize)
@@ -48,10 +60,12 @@ object DrawUtil {
     g2d.dispose
   }
 
-  private def drawSyllable(g2d: Graphics2D, syl: Syllable, rot: Double, sizeRatio: Double): Unit = {
+  private def drawSyllable(g2d: Graphics2D, syl: Syllable, rot: Double, sizeRatio: Double): Set[Coord] = {
     def isDouble(i: Int, syl: Syllable): Boolean = i > 0 && syl(i) == syl(i - 1)
     syl.indices.foreach(i => drawCharacter(g2d, syl(i), getSyllableCircle(syl, sizeRatio), isDouble(i, syl), sizeRatio))
+    val connectors = syl.indices.map(i => drawCharacter(g2d, syl(i), getSyllableCircle(syl, sizeRatio), isDouble(i, syl), sizeRatio))
     g2d.rotate(Math.toRadians(-rot), getWordCircle.center.x, getWordCircle.center.y)
+    connectors.flatten.toSet
   }
 
   private def getWordCircle(): Circle = {
@@ -78,16 +92,15 @@ object DrawUtil {
     Circle(fixedCenter, fixedRadius)
   }
 
-  private def drawCharacter(g2d: Graphics2D, c: Character, sylCircle: Circle, isDouble: Boolean, sizeRatio: Double): Unit = {
+  private def drawCharacter(g2d: Graphics2D, c: Character, sylCircle: Circle, isDouble: Boolean, sizeRatio: Double): Set[Coord] = {
     c match {
       case con: Consonant => drawConsonant(g2d, con, isDouble, sizeRatio)
       case vow: Vowel => drawVowel(g2d, vow, sylCircle, isDouble)
       case pun: Punctation => drawPunctation(g2d, pun)
     }
-    drawHelpPoint(g2d, getWordCircle.center)
   }
 
-  private def drawConsonant(g2d: Graphics2D, con: Consonant, isDouble: Boolean, sizeRatio: Double): Unit = {
+  private def drawConsonant(g2d: Graphics2D, con: Consonant, isDouble: Boolean, sizeRatio: Double): Set[Coord] = {
     val wc = getWordCircle
     val circle = getConsonantCircle(con, wc, isDouble, sizeRatio)
     val (c1, c2) = CalcUtil.calcStartAndEnd(wc, circle)
@@ -102,34 +115,62 @@ object DrawUtil {
     } else {
       drawCircle(g2d, circle, false)
     }
-    if (!isDouble) {
+    val connectors: Set[Coord] = if (!isDouble) {
       val da = Math.toRadians(10D)
       con.markType match {
-        case MarkType.NONE =>
+        case MarkType.NONE => Set.empty
         case MarkType.DOUBLE_DOT =>
           val (offset, size) = getOffsetAndSize(circle, con)
           drawConsonantPoint(g2d, getDot(circle, offset + da), size)
           drawConsonantPoint(g2d, getDot(circle, offset - da), size)
+          Set.empty
         case MarkType.TRIPPLE_DOT =>
           val (offset, size) = getOffsetAndSize(circle, con)
           drawConsonantPoint(g2d, getDot(circle, offset + (da * 2D)), size)
           drawConsonantPoint(g2d, getDot(circle, offset), size)
           drawConsonantPoint(g2d, getDot(circle, offset - (da * 2D)), size)
+          Set.empty
         case MarkType.TRIPPLE_LINE =>
           val angle = da * 2D
           drawLine(g2d, getLineEnd(circle, angle), getLineEnd(wc, angle))
           drawLine(g2d, getLineEnd(circle, 0D), getLineEnd(wc, 0D))
           drawLine(g2d, getLineEnd(circle, -angle), getLineEnd(wc, -angle))
+          Set(getLineEnd(circle, angle), getLineEnd(circle, 0D), getLineEnd(circle, -angle))
         case MarkType.LINE =>
           drawLine(g2d, getLineEnd(circle, 0D), getLineEnd(wc, 0D))
+          Set(getLineEnd(circle, 0D))
         case MarkType.DOUBLE_LINE =>
           drawLine(g2d, getLineEnd(circle, da), getLineEnd(wc, da))
           drawLine(g2d, getLineEnd(circle, -da), getLineEnd(wc, -da))
+          Set(getLineEnd(circle, da), getLineEnd(circle, -da))
       }
-    }
-    drawHelpPoint(g2d, c1)
-    drawHelpPoint(g2d, c2)
-    drawHelpPoint(g2d, circle.center)
+    } else { Set.empty }
+    connectors
+  }
+
+  private def drawVowel(g2d: Graphics2D, vow: Vowel, sylCircle: Circle, isDouble: Boolean): Set[Coord] = {
+    val wc = getWordCircle
+    val offset = (sylCircle.radius * vow.position.offset).intValue
+    val center = Coord(sylCircle.center.x, sylCircle.center.y + offset)
+    val rat = if (isDouble) { vow.position.doubleRatio } else { vow.position.ratio }
+    val radius = (sylCircle.radius * rat).intValue
+    val connectors: Set[Coord] = if (!isDouble) {
+      if (vow.position.equals(VowelPosition.CENTER_IN)) {
+        val from = Coord(center.x, center.y - radius)
+        val to = Coord(wc.center.x, wc.center.y - wc.radius)
+        drawLine(g2d, from, to)
+        highlightLineConnector(g2d, from)
+        Set(from)
+      } else if (vow.position.equals(VowelPosition.CENTER_OUT)) {
+        val from = Coord(center.x, center.y + radius)
+        val to = Coord(center.x, center.y + wc.radius)
+        drawLine(g2d, from, to)
+        highlightLineConnector(g2d, from)
+        Set.empty
+      } else { Set.empty }
+    } else { Set.empty }
+    drawCircle(g2d, Circle(center, radius), false)
+    connectors
   }
 
   private def getOffsetAndSize(circle: Circle, con: Consonant): (Double, Int) = {
@@ -146,37 +187,26 @@ object DrawUtil {
     Coord(x, y)
   }
 
+  private def getRotatedDot(coord: Coord, center: Coord, angle: Double): Coord = {
+    val xDiff = (coord.x - center.x)
+    val yDiff = (coord.y - center.y)
+    val cosA = Math.cos(Math.toRadians(angle))
+    val sinA = Math.sin(Math.toRadians(angle))
+    val x = center.x + (cosA * xDiff) - (sinA * yDiff)
+    val y = center.y + (sinA * xDiff) + (cosA * yDiff)
+    Coord(x.intValue, y.intValue)
+  }
+
   private def getLineEnd(circle: Circle, angle: Double): Coord = {
     val x = circle.center.x - (Math.sin(angle) * circle.radius).intValue
     val y = circle.center.y - (Math.cos(angle) * circle.radius).intValue - HALF_LINE
     Coord(x, y)
   }
 
-  private def drawVowel(g2d: Graphics2D, vow: Vowel, sylCircle: Circle, isDouble: Boolean): Unit = {
-    val wc = getWordCircle
-    val offset = (sylCircle.radius * vow.position.offset).intValue
-    val center = Coord(sylCircle.center.x, sylCircle.center.y + offset)
-    val rat = if (isDouble) { vow.position.doubleRatio } else { vow.position.ratio }
-    val radius = (sylCircle.radius * rat).intValue
-    if (!isDouble) {
-      if (vow.position.equals(VowelPosition.CENTER_IN)) {
-        val from = Coord(center.x, center.y - radius)
-        val to = Coord(wc.center.x, wc.center.y - wc.radius)
-        drawLine(g2d, from, to)
-      }
-      if (vow.position.equals(VowelPosition.CENTER_OUT)) {
-        val from = Coord(center.x, center.y + radius)
-        val to = Coord(center.x, center.y + wc.radius)
-        drawLine(g2d, from, to)
-      }
-    }
-    drawCircle(g2d, Circle(center, radius), false)
-    drawHelpPoint(g2d, center)
-  }
-
-  private def drawPunctation(g2d: Graphics2D, pun: Punctation): Unit = {
+  private def drawPunctation(g2d: Graphics2D, pun: Punctation): Set[Coord] = {
     //val wc = getWordCircle
     //TODO implement
+    Set.empty
   }
 
   private def drawConsonantPoint(g2d: Graphics2D, pos: Coord, size: Int): Unit = {
@@ -209,18 +239,14 @@ object DrawUtil {
     g2d.drawLine(from.x, from.y, to.x, to.y)
   }
 
-  private def drawHelpPoint(g2d: Graphics2D, pos: Coord): Unit = {
-    if (conceptMode) { drawPoint(g2d, pos, Color.WHITE) }
-  }
-
-  private def drawConnectorPoint(g2d: Graphics2D, pos: Coord): Unit = {
+  private def highlightLineConnector(g2d: Graphics2D, pos: Coord): Unit = {
     if (conceptMode) { drawPoint(g2d, pos, Color.RED) }
   }
 
   private def drawPoint(g2d: Graphics2D, pos: Coord, color: Color): Unit = {
     val oldColor = g2d.getPaint
     g2d.setPaint(color)
-    g2d.fill(new Ellipse2D.Double(pos.x - 1, pos.y - 1, 2, 2))
+    g2d.fill(new Ellipse2D.Double(pos.x - 2D, pos.y - 2D, 4D, 4D))
     g2d.setPaint(oldColor)
   }
 }
